cb9850c49c7a9d4163308c93871d42947229dfc0
matan <you@example.com>
2022-06-05 19:06:11 +0300

    add a unique verification email if there is an error you will see it, if there is an error do not crash the server

--- models/User.js
+++ models/User.js
@@ -1,41 +1,47 @@
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     userName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     userPhone: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     userEmail: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: [true, "Please enter your email address"],
+      unique: true,
+      lowercase: true,
+      validate: [isEmail, "Please enter a valid email address"],
     },
     password: {
-        type: String,
-        required: true,
-        minlength: [6, "minimum password length is 6 characters"]
+      type: String,
+      required: true,
+      minlength: [6, "minimum password length is 6 characters"],
     },
     vip: {
-        type: Boolean,
-        required: true
+      type: Boolean,
+      required: true,
     },
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
-userSchema.pre('save', async function(next) {
-    try {
-        const salt = await bcrypt.genSalt(10);
-        const hashPass = await bcrypt.hash(this.password, salt)
-        this.password = hashPass
-        next()
-    } catch (error) {
-        next(error)
-    }
-})
+userSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashPass = await bcrypt.hash(this.password, salt);
+    this.password = hashPass;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
