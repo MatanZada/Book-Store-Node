39c01ad21fb071410fac078a9f0cb94d3bb06ccb
snir2912 <89028623+snir2912@users.noreply.github.com>
2022-06-13 18:45:44 +0300

    create logger feture

--- logger/logger.js
+++ logger/logger.js
@@ -1,9 +1,35 @@
-const winston = require('winston');
+const {
+    format,
+    createLogger,
+    transports
+} = require('winston'), {
+    timestamp,
+    combine,
+    errors,
+    printf
+} = format
+chalk = require('chalk');
 
-const logger = winston.createLogger({
-    format: winston.format.simple(),
+const logFormat = printf(({
+    level,
+    message,
+    stack,
+    timestamp
+}) => {
+    return `${timestamp} ${level}: ${stack || message}`
+})
+const logger = createLogger({
+    level: 'debug',
+    format: combine(format.colorize(), timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        errors({
+            stack: true
+        }),
+        logFormat),
     defaultMeta: {
         service: 'user-service'
     },
-    transports: [],
+    transports: [new transports.Console()],
 });
+module.exports = logger
