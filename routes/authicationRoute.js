b53a96eee44c1c59440e1242f499d4385c846867
snir2912 <89028623+snir2912@users.noreply.github.com>
2022-07-09 16:46:19 +0300

    login

--- routes/authicationRoute.js
+++ routes/authicationRoute.js
@@ -1,6 +1,6 @@
 const Joi = require("joi");
 const bcrypt = require("bcrypt");
-const { User } = require("../models/user");
+const { User } = require("../models/User");
 const express = require("express");
 const router = express.Router();
 
